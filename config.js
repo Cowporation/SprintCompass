7f91da85e14b39711247a23803d1dc3147cad488
Zitong Wang <zwang452@gmail.com>
2022-03-10 15:24:00 -0500

    added db methods

---
+++ config.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+module.exports = {
+ atlas: process.env.DBURL,
+ appdb: process.env.DB,
+ projectsCollection: process.env.PROJECT_COLLECTION,
+ usersCollection: process.env.USER_COLLECTION,
+ listsCollection: process.env.LIST_COLLECTION,
+ tasksCollection: process.env.TASK_COLLECTION,
+ subtasksCollection: process.env.SUBTASK_COLLECTION,
+ port: process.env.PORT,
+};
